5dbe06e297a84cfb641caf215e7a2e5a6173fd71
DipanshuShora <deepanshushora478@gmail.com>
2018-06-20 00:04:47 +0530

    oops concept

--- this2.java
+++ this2.java
@@ -14,7 +14,7 @@ class School{
   }
   public static void main(String[] args){
     School S1 = new School(24,"Dipanshu","BCA");
-    School S2 = new School(78,"Sehej Sharma","BCA",86f);
+    School S2 = new School(8161629,"Deepanshu","BCA",78f);
     System.out.println(S1.rollno+" "+S1.name+" "+S1.course);
     System.out.println(S2.rollno+" "+S2.name+" "+S2.course+" "+S2.fee);
   }
